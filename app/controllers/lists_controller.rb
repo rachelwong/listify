34ab7c8ca81fb8e8333bc252ff15b9ea89674704
rachelwong <hello.rachelwong@gmail.com>
2019-04-05 10:55:26 +1000

    added groceries to view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,21 @@
 class ListsController < ApplicationController
-    def shopping
+
+    def new
+        @list = Lists.new
     end
+
+    def index
+    end
+
+    def shopping
+        @groceries = {"Bread" => 2, "Milk"=>2}
+    end
+
+    def show
+        # render plain: @list.inspect
+    end
+
+    # def packing
+    #     @clothes = {"Tee Shirts" => 3, "Pants" => 2, "Jackets" => 2, "Socks" => 12}
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :lists
+
+  # load the controller lists and use the index method
+  # root "lists#index"
+
+  # load the index page
+  root to: "lists#index"
+  get "shopping" => "lists#shopping"
 end
